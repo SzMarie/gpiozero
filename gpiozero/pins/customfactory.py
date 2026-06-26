f56063b8daa9262cbfed4f53fd7003e7b1d6ed15
marie <168172338+SzMarie@users.noreply.github.com>
2026-06-26 16:18:19 +0200

    ajout du relay carte multiio customfactory.py

--- gpiozero/pins/customfactory.py
+++ gpiozero/pins/customfactory.py
@@ -1,6 +1,21 @@
 from .lgpio import LGPIOFactory
+from megaind 
+from multiio import SMmultiio
 
-class customFactory(LGPIOFactory):
+class SequentRelay():
+  """
+  """
+  def _init_():
+    card = SMmultiio(stack=stack)
+    
+  def turn_on_relay(stack, channel):
+    card.set_relay(channel, 1)
+
+  def turn_off_relay(stack, channel):
+    card.set_relay(channel, 0)
+    
+
+class CustomFactory(LGPIOFactory):
   """
 
   """
