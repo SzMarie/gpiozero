afd13d87830d1a8316b2e9d551f42d5b26ac0796
marie <168172338+SzMarie@users.noreply.github.com>
2026-06-26 17:10:39 +0200

    Update customfactory.py

--- gpiozero/pins/customfactory.py
+++ gpiozero/pins/customfactory.py
@@ -5,14 +5,14 @@ from multiio import SMmultiio
 class SequentRelay():
   """
   """
-  def _init_():
-    card = SMmultiio(stack=stack)
+  def _init_(self, stack=0):
+    self.card = SMmultiio(stack=stack)
     
-  def turn_on_relay(stack, channel):
-    card.set_relay(channel, 1)
+  def turn_on_relay(self, channel):
+    self.card.set_relay(channel, 1)
 
-  def turn_off_relay(stack, channel):
-    card.set_relay(channel, 0)
+  def turn_off_relay(self, channel):
+    self.card.set_relay(channel, 0)
     
 
 class CustomFactory(LGPIOFactory):
